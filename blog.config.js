c116587a6b953cccd7c47e8f2c67a2795b897d6e
LoganWang0407 <wlg453288@163.com>
2021-03-21 14:49:18 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'ULTRAFUNS',
+  author: '有缺方思',
+  email: 'wlg453288@163.com',
+  link: 'https://nobelium-loganwang.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'zh-CN',
   appearance: 'auto', // ['light', 'dark', 'auto'],
@@ -13,7 +13,7 @@ const BLOG = {
   postsPerPage: 7,
   showAbout: true, // WIP
   showArchive: true, // WIP
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://twitter.com/LiuG_W',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
